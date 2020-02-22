066406acca54aeaf3452b4c9ce45d79e8362eac7
unknown <yonoraphael@gmail.com>
2020-02-23 05:35:48 +0700

    Updates

---
+++ precache-manifest.008a62b2e7b147345ce93cb90b30e08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056b50cdedaafd9eefb9086d13079708",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "d2a4e210db070bc82dda",
+    "url": "/business/static/css/main.e98867b1.chunk.css"
+  },
+  {
+    "revision": "6d3f97db50adb7503fb1",
+    "url": "/business/static/js/2.f18308fe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/business/static/js/2.f18308fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a4e210db070bc82dda",
+    "url": "/business/static/js/main.7f2e3197.chunk.js"
+  },
+  {
+    "revision": "a9df0607912f9e02ce00",
+    "url": "/business/static/js/runtime-main.8686939d.js"
+  },
+  {
+    "revision": "35f2d712f23aed525776eda4a94cc9d7",
+    "url": "/business/static/media/hero.35f2d712.jpg"
+  }
+]);
